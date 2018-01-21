4206927841583481ca5371c1fbcae9d5127ee3d8
floatrush <conrad.etzel@gmail.com>
2018-01-20 17:25:39 -0800

    Created Basic Notes Model

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,24 @@
+class NotesController < ApplicationController
+  def new
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    @note.save
+    redirect_to @note
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def index
+    @notes = Note.all
+  end
+
+  private
+    def note_params
+      params.require(:note).permit(:book, :chapter, :start_verse, :end_verse, :note_text)
+    end
+end
